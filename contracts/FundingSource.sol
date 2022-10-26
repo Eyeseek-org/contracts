fcb91258fe5aa6a7b8de3af41a6db4a29f2dd736
kazii <kazdan@seznam.cz>
2022-10-26 15:26:52 +0200

    Preparation for Axelar

--- contracts/FundingSource.sol
+++ contracts/FundingSource.sol
@@ -6,28 +6,26 @@ import {IAxelarGasService} from "./interfaces/IAxelarGasService.sol";
 import {IAxelarGateway} from "./interfaces/IAxelarGateway.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-
-
 contract FundingSource is IAxelarExecutable {
-    IAxelarGasService gasReceiver;
+     IAxelarGasService gasReceiver;
+     IAxelarGateway immutable gateway;
 
-    constructor(address _gateway, address _gasReceiver)
-        IAxelarExecutable(_gateway)
-    {
+    constructor(address _gateway, address _gasReceiver) {
+        gateway = IAxelarGateway(_gateway);
         gasReceiver = IAxelarGasService(_gasReceiver);
     }
 
     function callWithToken(
-        string memory destinationChain,
-        string memory destinationAddress,
-        bytes memory payload,
-        string memory symbol,
+        string calldata destinationChain,
+        string calldata destinationAddress,
+        string calldata symbol,
         uint256 amount
     ) external payable {
         address tokenAddress = gateway.tokenAddresses(symbol);
         IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
         IERC20(tokenAddress).approve(address(gateway), amount);
-
+        // Encode number going to Direct Donation, amount to Microfund
+        bytes memory payload = abi.encode(destinationAddresses); // TO REPLACE
         if (msg.value > 0) {
             gasReceiver.payNativeGasForContractCallWithToken{value: msg.value}(
                 address(this),
